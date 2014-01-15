0a6a3ff22351f78780a19e28a41124295c430e18
Andrew Stiles <aostiles@gmail.com>
2014-01-14 20:28:25 -0800

    add preliminary db stuff

--- go/server.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/projects")
-}
--- server/server.go
+++ server/server.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/astaxie/beedb"
+	_ "github.com/mattn/go-sqlite3"
+	"database/sql"
+)
+
+type Tag struct {
+	Uid int
+	Name string
+}
+
+var orm beedb.Model
 
 func main() {
-	fmt.Printf("Hello World!.\n")
+	db, err := sql.Open("sqlite3", "./main.db")
+	if err != nil {
+		panic(err)
+	}
+	orm = beedb.New(db)
+	insertTag()
+}
+
+func insertTag() {
+	var mytag Tag
+	mytag.Name = "First Tag"
+	orm.Save(&mytag)
+	fmt.Println(mytag)
 }
